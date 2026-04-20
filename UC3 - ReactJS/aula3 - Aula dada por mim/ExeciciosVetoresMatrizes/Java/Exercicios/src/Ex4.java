3c602368497c096bf8ebb8e78de1e4fb933dd20c
EuAndersonDev <andersonaugustorei1227@gmail.com>
2026-04-20 19:56:19 -0300

    commet code

--- UC3 - ReactJS/aula3 - Aula dada por mim/ExeciciosVetoresMatrizes/Java/Exercicios/src/Ex4.java
+++ UC3 - ReactJS/aula3 - Aula dada por mim/ExeciciosVetoresMatrizes/Java/Exercicios/src/Ex4.java
@@ -1,9 +1,16 @@
+﻿// Importa dependencias necessarias para o codigo.
 import java.util.*;
 
+// Define a classe principal do arquivo.
 public class Ex4 {
+    // Define o metodo principal de execucao.
     public static void main(String[] args) {
+        // Cria um leitor de entrada para dados do usuario.
         Scanner in = new Scanner(System.in);
 
+        // Inicia um laco de repeticao.
         for(int i = 0; i<)
+    // Fecha um bloco de codigo.
     }
+// Fecha um bloco de codigo.
 }
--- UC3 - ReactJS/aula3 - Aula dada por mim/ExeciciosVetoresMatrizes/Java/untitled/src/Main.java
+++ UC3 - ReactJS/aula3 - Aula dada por mim/ExeciciosVetoresMatrizes/Java/untitled/src/Main.java
@@ -1,13 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+﻿//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Executa esta instrucao do exercicio.
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
+    // Executa esta instrucao do exercicio.
     IO.println(String.format("Hello and welcome!"));
 
+    // Inicia um laco de repeticao.
     for (int i = 1; i <= 5; i++) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        // Executa esta instrucao do exercicio.
         IO.println("i = " + i);
+    // Fecha um bloco de codigo.
     }
+// Fecha um bloco de codigo.
 }
